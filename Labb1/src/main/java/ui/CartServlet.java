8a41ae4a102e8657cfa653a5c458ccdc6ad26e32
Beatrice Poriazov <bettypori@gmail.com>
2024-10-05 18:55:03 +0200

    Temporary solution to not add more items to cart than available

--- Labb1/src/main/java/ui/CartServlet.java
+++ Labb1/src/main/java/ui/CartServlet.java
@@ -19,7 +19,6 @@ public class CartServlet extends HttpServlet {
 
         BookInfo bookInfo = BookHandler.getBookByItemId(Integer.parseInt(itemIdStr));
         OrderItemInfo orderItemInfo = new OrderItemInfo(bookInfo, bookInfo.getItemId(), 1);
-
         if (cart == null) {
             cart = new ArrayList<>();
             session.setAttribute("cart", cart);
@@ -29,13 +28,17 @@ public class CartServlet extends HttpServlet {
 
         for (OrderItemInfo item : cart) {
             if (item.getItemId() == orderItemInfo.getItemId()) {
-                item.setNrOfItems(item.getNrOfItems() + 1);
+                if (item.getItem().getNrOfCopies() != 1) {
+                    item.setNrOfItems(item.getNrOfItems() + 1);
+                    item.getItem().setNrOfCopies(item.getItem().getNrOfCopies() - 1);
+                }
                 existingItem = true;
                 break;
             }
         }
         if (!existingItem) {
-            cart.add(orderItemInfo);
+            if (orderItemInfo.getItem().getNrOfCopies() != 1)
+                cart.add(orderItemInfo);
         }
         response.sendRedirect("shop.jsp");
     }
